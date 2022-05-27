5e4da70c24484e61f58bc43e53dfc64455161767
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-27 11:34:44 +0600

    purahase ditalies  add mongodb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -16,14 +16,33 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
   try{
+    
     await client.connect();
     const toolsCollection = client.db("manufacture_tools").collection("tools") ;
-    console.log('mongo db conneated')
+    const purchaseCollection = client.db("manufacture_tools").collection("purchases") ;
 
+    // get all tools load database  api
     app.get('/tools', async(req, res) => {
       const query = {} ;
       const result = await toolsCollection.find(query).toArray();
       res.send(result);
+    })
+
+
+    // get on tool load database  api 
+    app.get('/tools/:id', async(req, res) => {
+      const id = req.params.id ;
+      const query = {_id : ObjectId(id)} ;
+      const tool = await toolsCollection.findOne(query) ;
+      res.send(tool);
+    })
+
+    // all purchase post is api 
+    app.post('/purchase', async(req, res) => {
+      const purchase = req.body ;
+      const result = await purchaseCollection.insertOne(purchase) ;
+      res.send(result)
+      console.log(purchase)
     })
 
     
